eae8e56a71202a951911cfae2d5e7125149fcea6
Danny <danny@pagelines.com>
2014-10-05 11:52:20 +0100

    PageLines Framework branch
    Includes ContentBox section

--- functions.php
+++ functions.php
@@ -1,8 +1,8 @@
 <?php
 /**
  * Danny's Skeleton child theme
- * Containing nothing other than 
- * the bare bone files 
+ * Containing nothing other than
+ * the bare bone files
  */
 
 // Add your functions below
